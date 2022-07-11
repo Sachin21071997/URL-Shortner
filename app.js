89dc65af96f759e3459b720934493f494c43ce3c
beejawat.1@iitj.ac.in <=>
2022-07-11 21:46:01 +0530

    my third commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3002;
 app.use(express.json());
 app.use(cors());
+
 // Database config
 const connection = require('./config/config')
 connection.once('open', () => console.log('DB Connected'))
